9a64e8e1de15ac700379b4704c83841ba99431f8
sidharth3000 <2019522342iiitvadodara.ac.in>
2020-05-13 01:53:01 +0530

    Updates

---
+++ precache-manifest.54a25972691f72c1312ad35a98ba51ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "115936012586d10f3bf62e7d82410c37",
+    "url": "/ezewev/index.html"
+  },
+  {
+    "revision": "1fc919ebf4cda6bd975d",
+    "url": "/ezewev/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "32c517635f4c94ccbaaa",
+    "url": "/ezewev/static/css/main.28ddbf6f.chunk.css"
+  },
+  {
+    "revision": "1fc919ebf4cda6bd975d",
+    "url": "/ezewev/static/js/2.972aa3c4.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/ezewev/static/js/2.972aa3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c517635f4c94ccbaaa",
+    "url": "/ezewev/static/js/main.0c5f454e.chunk.js"
+  },
+  {
+    "revision": "3651654d8ecff2008199",
+    "url": "/ezewev/static/js/runtime-main.79be8037.js"
+  }
+]);
